22cb7e68b341aaa0417da25fe65c7bf2c324989e
htao <htao@student.chalmers.se>
2021-04-20 21:03:42 +0200

    Started with fill in the time slots

--- frontend/src/components/Personnel.vue
+++ frontend/src/components/Personnel.vue
@@ -1,25 +1,25 @@
-<template>
+o<template>
 <div>
   <b-button v-on:click="addRow(); fillInTime()"> hej </b-button>
   <div v-for="personnel in personnelList" :key="personnel">
-    <b-row>
+    <b-row :id="personnel.id" >
       <b-col id='name' class='names'>{{personnel.personnelName}} </b-col>
       <b-col id='col1' ref="col1"></b-col>
-      <b-col id='2'></b-col>
-      <b-col id='3'></b-col>
-      <b-col id='4'></b-col>
-      <b-col id='5'></b-col>
-      <b-col id='6'></b-col>
-      <b-col id='7'></b-col>
-      <b-col id='8'></b-col>
-      <b-col id='9'></b-col>
-      <b-col id='10'></b-col>
-      <b-col id='11'></b-col>
-      <b-col id='12'></b-col>
-      <b-col id='13'></b-col>
-      <b-col id='14'></b-col>
-      <b-col id='15'></b-col>
-      <b-col id='16'></b-col>
+      <b-col id='col2'></b-col>
+      <b-col id='col3'></b-col>
+      <b-col id='col4'></b-col>
+      <b-col id='col5'></b-col>
+      <b-col id='col6'></b-col>
+      <b-col id='col7'></b-col>
+      <b-col id='col8'></b-col>
+      <b-col id='col9'></b-col>
+      <b-col id='col10'></b-col>
+      <b-col id='col11'></b-col>
+      <b-col id='col12'></b-col>
+      <b-col id='col13'></b-col>
+      <b-col id='col14'></b-col>
+      <b-col id='col15'></b-col>
+      <b-col id='col16'></b-col>
     </b-row>
   </div>
 </div>
@@ -27,15 +27,20 @@
 
 <script>
 
+var i = 0
+var j = 3
+
 export default {
   name: 'personnel',
   data: function () {
     return {
       personnelList: [{
+        id: '1',
         personnelName: 'Henrik.T',
-        startTime: '1',
-        endTime: '3'
+        startTime: '3',
+        endTime: '9'
       }, {
+        id: '2',
         personnelName: 'Henrik.L',
         startTime: '5',
         endTime: '8'
@@ -44,19 +49,35 @@ export default {
   },
   methods: {
     fillInTime: function () {
-      var j = 0
-      for (j = this.startTime; this.startTime < this.endTime; j++) {
-        console.log('PATRIK')
-        const col = document.getElementById('col' + this.startTime)
+      for (var x = this.personnelList[i].startTime; x < this.personnelList[i].endTime; x++) {
+        const col = document.getElementById('col' + x)
         col.style = 'background-color: #e3e3e3'
       }
+      var numberOfSiblings = document.getElementById('row').parentElement.childElementCount
+
+      var base = "document.getElementById('row').parentElement"
+      for (var y = 0; y < numberOfSiblings; y++) {
+        base = base.concat('.nextElementSibling')
+      }
+      base = base.concat('.firstChild')
+      base = base.children[5]
+      base.style = 'background-color: #e3e3e3'
+
+      /*
+      var test = document.getElementById('row').parentElement.nextElementSibling.firstChild
+      console.log(test)
+      var test1 = test.children[15]
+      test1.style = 'background-color: #e3e3e3'
+      */
     },
     addRow: function () {
       var object = {
+        id: j,
         personnelName: 'NAME',
         startTime: '1',
         endTime: '3'
       }
+      j++
       this.personnelList.push(object)
       this.personnelName = ''
       this.startTime = ''
